90ecbda5235f3943c0182a81207d190a5460ec38
Mbote-Joseph <mbotejoseph001@gmail.com>
2021-02-07 17:08:41 +0300

    Commiting the Material UI repo

--- js/loader.js
+++ js/loader.js
@@ -14,3 +14,32 @@ function toggleModal() {
   var instance = M.Modal.getInstance($("#modal3"));
   instance.open();
 }
+
+$(".fixed-action-btn").floatingActionButton({
+  toolbarEnabled: true,
+});
+
+// FAB
+// var options = [
+//   {
+//     icon: "1",
+//     label: "Run alert function",
+//     callback: function () {
+//       alert("dasd");
+//     },
+//   },
+//   {
+//     icon: '<i class="fa fa-id-card" aria-hidden="true"></i>',
+//     label: "Test Element 2",
+//   },
+//   {
+//     icon: "3",
+//     label: "Test Element 3",
+//     display: {
+//       color: "blue",
+//       background: "red",
+//     },
+//   },
+// ];
+
+// var context = new Bubbler(options);
